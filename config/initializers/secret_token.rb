bb86442a8df063a19e92dca0a0fece6beb5e47b5
Elton Chung <elbo1216@gmail.com>
2012-05-19 14:16:20 -0700

    Base Rails Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sissichan2012::Application.config.secret_token = '0761e5ba5c366f158176a5a458e850e84848b5be66180978787256e4f9fa79906fc8fe7fb84ff737e78460b554bc91c31b94011deacae0c16a80cc389d3e4f5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sissichan2012::Application.config.session_store :cookie_store, :key => '_sissichan_2012_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sissichan2012::Application.config.session_store :active_record_store
