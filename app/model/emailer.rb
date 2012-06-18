34731cdec32d3a667c0d926f1199674c0d4051a2
Elton Chung <elbo1216@gmail.com>
2012-06-17 21:41:13 -0400

    Another emailer fix.

--- app/model/emailer.rb
+++ app/model/emailer.rb
@@ -2,12 +2,12 @@ class Emailer < ActionMailer::Base
   default :to => 'elbo1216@gmail.com'
 
   def contact(name, email, subject, message, eventDate)
-    mail(:subject => subject,
-         :from => email)
-   
     @message = message
     @eventDate = eventDate
     @name = name
     @email = email
+
+    mail(:subject => subject,
+         :from => email)
   end
 end
