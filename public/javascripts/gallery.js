a775034071ee37243267df43fa07b994ac9db172
Elton Chung <elbo1216@gmail.com>
2012-06-21 23:43:14 -0400

    Minor fixes from user found bugs.  Added image level pagination to gallery.

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,32 +1,67 @@
-function showImage(imagePath, caption, url) {
-  var container = $('.full-image .image-container')
-  container.empty();
-  container
+function showImage(imageId, totalImages, imagePath, caption, url) {
+  var imageContainer = $('.full-image .image-container')
+  imageContainer.empty();
+  imageContainer
      .css('background-image', "url(" + imagePath + ")")
      .css('background-repeat', "no-repeat")
      .css('width', '435px')
      .css('height', '405px')
-     .css('textAlign', 'center')
-  if (url != "") {
-    var caption = $('<a>')
-                     .attr('href', url)
-                     .attr('target', '_blank')
-                     .css('color', '#FEF3F6')
-                     .css('font-weight', 'bold')
-                     .hover(function() {
-                       $(this).css('text-decoration', 'underline')
-                      }, function() {
-                       $(this).css('text-decoration', 'none')
-                      }
-                     )
-                     .html(caption);
-    var span = $('<div>')
-                 .css('height', '100%')
-                 .css('width', '100%')
-                 .css('paddingTop', '390px')
-                 .append(caption);
-    container.append(span);
+     .css('textAlign', 'left')
+
+
+  /* 
+   * Logic to create next/prev photo on large image.  Show left arrow except on  
+   * first image.  Show right arrow except on last image.  If the next image
+   * belongs on the next/prev page, adjust pagination.
+   */
+  // Left Link
+  if (imageId > 1) {
+    $('#image-nav-prev')
+     .attr('href', $($('#thumb-'+(imageId-1)).find("a")[0]).attr('href'));
   }
+
+  // Right Link
+  if (imageId != totalImages) {
+    $('#image-nav-next')
+     .attr('href', $($('#thumb-'+(imageId+1)).find("a")[0]).attr('href'));
+  }
+
+  var captionLink = caption
+  if (url != "") {
+    captionLink = $('<a>')
+                   .attr('href', url)
+                   .attr('target', '_blank')
+                   .css('color', '#FEF3F6')
+                   .css('font-weight', 'bold')
+                   .hover(function() {
+                     $(this).css('text-decoration', 'underline')
+                    }, function() {
+                     $(this).css('text-decoration', 'none')
+                    }
+                   )
+                   .html(caption);
+
+  }
+  var imageDiv = $('<div>')
+               .css('position', 'absolute')
+               .css('float', 'left')
+               .css('font-weight', 'bold')
+               .css('paddingTop', '385px')
+               .css('paddingLeft', '10px')
+               .append(captionLink);
+  var imageDivShadow = $('<div>')
+               .css('position', 'absolute')
+               .css('font-weight', 'bold')
+               .css('marginTop', '386px')
+               .css('marginLeft', '11px')
+               .css('color', 'black')
+               .append(caption);
+
+  imageContainer.append(imageDivShadow);
+  imageContainer.append(imageDiv);
+
+  // Need to reset the page that the thumbnail of the image belongs
+  showPage(parseInt($('#thumb-'+imageId).attr('class').split(" ")[1].substring(5)));
 }
 
 function showPage(page) {
